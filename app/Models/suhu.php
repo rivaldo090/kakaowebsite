540fa6a34b0d2a3eb7867be54cf1e6be0180fdd4
rivaldo090 <asrivaldo450@gmail.com>
2025-07-06 15:32:32 +0700

    Perubahan lokal sebelum rebase

--- app/Models/suhu.php
+++ app/Models/suhu.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Suhu extends Model
 {
+    use HasFactory;
+
+    protected $table = 'suhus'; // Pastikan ini sesuai dengan nama tabel di database
     protected $fillable = ['nilai'];
 }
