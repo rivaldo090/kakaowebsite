0f000d652499653a73dd0774cb6ef68c9e94c81c
rivaldo090 <asrivaldo450@gmail.com>
2025-07-06 10:38:47 +0700

    Perubahan lokal sebelum rebase

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::create([
+    'username' => 'testuser',
+    'email' => 'test@example.com',
+    'role' => 'user', // jika perlu
+    'password' => bcrypt('password'),
+]);
+
     }
 }
